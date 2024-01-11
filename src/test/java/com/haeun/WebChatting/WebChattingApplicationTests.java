027cd661599df633a7a8be93945588dde71f4e8e
HaEun <he.yang@jhcon.net>
2024-01-11 14:48:29 +0900

    기본 설정

---
+++ src/test/java/com/haeun/WebChatting/WebChattingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haeun.WebChatting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebChattingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
